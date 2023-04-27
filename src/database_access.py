90dd8e82ec7ad75949880389fbef0c60793c0d12
dvarkless <amir911157@yandex.ru>
2023-04-27 15:19:39 +0300

    Add methods, fix sql queries, other errors

--- src/database_access.py
+++ src/database_access.py
@@ -1,5 +1,8 @@
+import logging
 import sqlite3 as sql
 from pathlib import Path
+
+from setup_handler import get_handler
 
 
 class Database:
@@ -13,6 +16,14 @@ class Database:
         'user': 'VARCHAR(70)',
         'user_id': 'INTEGER',
         'trigger_blacklist': 'INTEGER',
+    }
+    __relevant_actions = {
+        'model': ['start', 'txt2img', 'img2img', 'set_model'],
+        'gen_mode': ['start', 'txt2img', 'img2img', 'rescale',
+                     'change_generation_mode'],
+        'prompt': ['start', 'txt2img', 'img2img'],
+        'oriention': ['start', 'txt2img', 'img2img',
+                      'change_orientation_mode'],
     }
 
     def __init__(self, path: str | Path, actions_txt_path: str | Path = ''):
@@ -29,10 +40,21 @@ class Database:
         self.is_blocked = False
         self.last_username = ''
         self.possible_actions = []
+
+        self.all_actions = set()
+        for actions in self.__relevant_actions.values():
+            self.all_actions |= set(actions)
+        self.all_actions = list(self.all_actions)
+
         if actions_txt_path != '':
             with open(actions_txt_path, 'r') as f:
                 for line in f:
                     self.possible_actions.append(line.strip())
+
+        self.logger = logging.getLogger(__name__)
+
+# add handler to logger
+        self.logger.addHandler(get_handler())
 
     def __enter__(self):
         self.con = sql.connect(self.path)
@@ -58,8 +80,9 @@ class Database:
         self.cur.execute(query)
         self.con.commit()
         self.con.close()
+        self.logger.debug('Created table')
 
-    def insert(self, action: str, user: User, gen_mode: int = -1,
+    def insert(self, action: str, user, gen_mode: int = -1,
                model: int = -1, orientation: int = -1,
                prompt: str = '', blocked: bool = False):
         if isinstance(user, int):
@@ -72,20 +95,18 @@ class Database:
 
         if self.possible_actions:
             assert action in self.possible_actions
+        keys_to_add = list(self.__table_contents.keys())
+        keys_to_add.remove('id')
+
+        questions_len = len(self.__table_contents) - 1
+        questions = '(' + ', '.join(['?' for _ in range(questions_len)]) + ');'
 
         query = 'INSERT INTO main ('
-        questions = '('
-        for key in self.__table_contents.keys():
-            if key == 'id':
-                continue
-            query += f'{key},\n'
-            questions += '?, '
-        query = query.rstrip(',\n')
-        questions = questions.rstrip(', ')
+        query += ', \n'.join(keys_to_add)
         query += ')\nVALUES '
-        questions += ');'
         query += questions
         self.last_query = query
+
         args = (
             action,
             model,
@@ -97,27 +118,68 @@ class Database:
             int(blocked),
         )
         self.cur.execute(query, args)
+        self.logger.debug(self.last_query)
 
-    def update_for_user(self, user):
+    def check_user_exists(self, user):
         if isinstance(user, int):
             user_id = user
         else:
             user_id = user.id
 
+        query = """
+            SELECT COUNT(*) FROM main WHERE user_id = ?;
+        """
+        self.cur.execute(query, (user_id,))
+
+        out = self.cur.fetchone()
+        return bool(out)
+
+    def update_for_user(self, user, update_only=None):
+        if update_only:
+            assert update_only in self.__relevant_actions.keys()
+        if isinstance(user, int):
+            user_id = user
+        else:
+            user_id = user.id
+
+        additional = ''
+        actions_str = '('
+        if update_only:
+            actions_str += ','.join([f"'{m}'" for m in
+                                    self.__relevant_actions[update_only]])
+        else:
+            actions_str += ','.join([f"'{m}'" for m in
+                                    self.all_actions])
+
+        actions_str += ')'
+        additional = f'AND action IN {actions_str}'
 
         query = f"""
             SELECT * FROM main
-            WHERE user_id={user_id}
+            WHERE user_id=? {additional}
             ORDER BY id DESC;
         """
-        self.cur.execute(query)
+        print(query)
+        self.cur.execute(query, (user_id,))
         out = self.cur.fetchone()
+
         if out:
-            self.last_action = out[1]
-            self.last_model = out[2]
-            self.last_gen_mode = out[3]
-            self.last_orientation = out[5]
-            self.is_blocked = True if out[8] > 1 else False
+            if not update_only:
+                _, self.last_action, self.last_model, \
+                    self.last_gen_mode, self.last_prompt, \
+                    self.last_orientation, _, \
+                    _, self.is_blocked = out
+                self.is_blocked = True if self.is_blocked > 1 else False
+            elif update_only == 'model':
+                self.last_model = out[2]
+            elif update_only == 'gen_mode':
+                self.last_gen_mode = out[3]
+            elif update_only == 'prompt':
+                self.last_prompt = out[4]
+            elif update_only == 'orientation':
+                self.last_orientation = out[5]
+
+        self.logger.debug(query)
 
     def select_all(self):
         query = "SELECT * FROM main"
@@ -137,6 +199,5 @@ if __name__ == '__main__':
     user.username = 'template'
     user.id = 101
     with Database(path) as db:
-        # db.insert('iii2img', 0, user, 0, 'create asd smth', False)
-        db.update_for_user(user)
+        db.insert("test", user)
         print(db.last_model)
